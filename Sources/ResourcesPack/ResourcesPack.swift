fb17b185ce663a84e927fe7bae897267c7c3fd6e
Ashamaz Shidov <ashamazsh@gmail.com>
2024-11-05 13:06:16 +0100

    Test bundle

--- Sources/ResourcesPack/ResourcesPack.swift
+++ Sources/ResourcesPack/ResourcesPack.swift
@@ -3,6 +3,14 @@
 
 import Foundation
 
-public let resBundle: URL? = Bundle.module.url(forResource: "Resources", withExtension: "bundle")
+public func resBundle() -> Bundle? {
+    guard let resourceUrl = Bundle.module.resourceURL?.appending(component: "Resources.bundle") else {
+        return nil
+    }
+    
+    return Bundle(url: resourceUrl)
+}
+
 public let compass: URL? = Bundle.module.url(forResource: "compass", withExtension: "png")
 public let js: URL? = Bundle.module.url(forResource: "example", withExtension: "js")
+
